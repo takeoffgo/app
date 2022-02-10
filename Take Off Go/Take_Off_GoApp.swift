fddcc4623b3d0b40bdda24fad834cfce489747b0
Brendan McKenzie <hello@brendanmckenzie.com>
2022-02-10 11:19:39 +1100

    Initial Commit

---
+++ Take Off Go/Take_Off_GoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Take_Off_GoApp.swift
+//  Take Off Go
+//
+//  Created by Brendan on 10/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct Take_Off_GoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
